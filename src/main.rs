c127915026621cfc7a5920c8d4830c94d27f158e
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-11-18 10:36:51 -0700

    Loops

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,42 @@ fn if_statement() {
         } else if temp < 10 {"cold"} else {"ok"});
 }
 
+fn while_and_loop() {
+    let mut x = 1;
+
+    while x < 1000 {
+        x *= 2;
+
+        if x == 64 { continue; }
+
+        println!("x = {}", x)
+    }
+
+    let mut y = 1;
+    loop {
+        y *= 2;
+        println!("y = {}", y);
+
+        if y == 1<<10 { break; }
+    }
+}
+
+fn for_loop() {
+    for x in 1..11 {
+        if x == 3 { continue; }
+
+        if x == 8 { break; }
+
+        println!("x = {}", x);
+    }
+
+    for (pos, y) in (30..41).enumerate() {
+        println!("{}: {}", pos, y);
+    }
+}
+
 fn main() {
-    if_statement();
+    //if_statement();
+    // while_and_loop();
+    for_loop();
 }
